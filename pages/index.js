c0cea877735af56730d49a634d8eba4f1f429183
Danny Vasta <danny@MacBook-Pro.local>
2023-07-18 16:17:11 -0400

    removing node version, updating column style

--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,33 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, my name is <strong>Danny Vasta</strong>! I'm a Software Engineer <i>(currently open for work)</i> with a passion for frontend/web development technologies. I care about code maintenance, user experience and accessibility.</p>
-        <p>Experienced with React, Redux, Javascript/Typescript/ES6 and Frontend Architecture design efforts.</p>
+        <p>Experienced with React, Redux, Javascript/Typescript and Frontend Architecture design efforts.</p>
       </section>
-
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Info</h2>
-        <ul className={utilStyles.list}>
-          <li className={utilStyles.listItem}>
-            <a href="/resume.pdf" target="_blank">Resume</a>
-          </li>
-          <li className={utilStyles.listItem}>
-            <a href="https://www.linkedin.com/in/daniel-vasta/" target="_blank">LinkedIn</a>
-          </li>
-          <li className={utilStyles.listItem}>
-            <a href="https://github.com/DNNYVST" target="_blank">Github</a>
-          </li>
-        </ul>
+      <section class="leftcolumn">
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Info</h2>
+          <ul className={utilStyles.list}>
+            <li className={utilStyles.listItem}>
+              <a href="/resume.pdf" target="_blank">Resume</a>
+            </li>
+            <li className={utilStyles.listItem}>
+              <a href="https://www.linkedin.com/in/daniel-vasta/" target="_blank">LinkedIn</a>
+            </li>
+            <li className={utilStyles.listItem}>
+              <a href="https://github.com/DNNYVST" target="_blank">Github</a>
+            </li>
+          </ul>
+        </section>
       </section>
-
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Contact</h2>
-        <ul className={utilStyles.list}>
-          <li className={utilStyles.listItem}>
-            <a href="mailto:djv711@gmail.com" target="_blank">E-mail</a>
-          </li>
-        </ul>
+      <section class="rightcolumn">
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Contact</h2>
+          <ul className={utilStyles.list}>
+            <li className={utilStyles.listItem}>
+              <a href="mailto:djv711@gmail.com" target="_blank">E-mail</a>
+            </li>
+          </ul>
+        </section>
       </section>
     </Layout>
   )
