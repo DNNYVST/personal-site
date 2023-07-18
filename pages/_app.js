8ce568bad8f8848407f4e497180b3be895e72c67
Danny Vasta <danny@MacBook-Pro.local>
2023-07-18 17:19:52 -0400

    switch to next local font

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/global.css'
 import { Analytics } from '@vercel/analytics/react';
+import localFont from 'next/font/local';
+
+const font = localFont({src: './iwata.otf'})
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={font.className}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </main>
   );
 }
