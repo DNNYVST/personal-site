aaf2ca5d73f67b99b5c2897b2bd780c0a9c04e53
Danny Vasta <djv711@gmail.com>
2024-05-07 15:08:36 -0400

    secret updates

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -1,28 +1,17 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 
 export default function Secret() {
   return (
     <>
-      <Layout home={false}>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
         <section className="twoSecondFadeIn">
-          <section className={utilStyles.headingMd}>
-            <p>
-              Hi there, my name is <strong>Danny</strong>! I'm a Software
-              Engineer <i>(currently open for work!)</i> with a passion for
-              frontend/web development technologies.
-            </p>
-            <p>
-              I am experienced with React, Redux, Javascript/Typescript and
-              frontend architecture design efforts.
-            </p>
-          </section>
+            <section>
+            <video autoPlay muted loop id="clouds">
+                <source src="/clouds.mp4" type="video/mp4" />
+            </video>
+            <video autoPlay muted loop id="thunderstorm_daisies">
+                <source src="/thunderstorm_daisies.mp4" type="video/mp4" />
+            </video>
+            </section>
         </section>
-      </Layout>
     </>
   );
 }
