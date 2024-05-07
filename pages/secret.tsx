c6f6974733682ad46093c696c106bbedcda59ae2
Danny Vasta <djv711@gmail.com>
2024-05-07 15:12:04 -0400

    update resume

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -4,12 +4,12 @@ export default function Secret() {
     <>
         <section className="twoSecondFadeIn">
             <section>
-            <video autoPlay muted loop id="clouds">
-                <source src="/clouds.mp4" type="video/mp4" />
-            </video>
-            <video autoPlay muted loop id="thunderstorm_daisies">
-                <source src="/thunderstorm_daisies.mp4" type="video/mp4" />
-            </video>
+              <video autoPlay muted loop id="clouds">
+                  <source src="/clouds.mp4" type="video/mp4" />
+              </video>
+              <video autoPlay muted loop id="thunderstorm_daisies">
+                  <source src="/thunderstorm_daisies.mp4" type="video/mp4" />
+              </video>
             </section>
         </section>
     </>
